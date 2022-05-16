4b843c64783d868adef0f150d02736c30fb6c872
zhengbifeng <zhengbifeng@bz.cn>
2022-05-16 12:50:55 +0800

    1、增加短信验证码模块 2、增加短信验证码路由 3、增加响应码功能

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -2,7 +2,7 @@
 # * 进行users子应用的视图路由
 from django.urls import path
 
-from users.views import RegisterView, ImageCodeView
+from users.views import RegisterView, ImageCodeView, SmsCodeView
 
 urlpatterns = [
     # 参数1： 路由
@@ -10,6 +10,8 @@ urlpatterns = [
     # 参数3： 路由名，方便通过reverse来获取路由
     path('register/', RegisterView.as_view(), name='register'),
     # 图片验证码的路由
-    path('imagecode/', ImageCodeView.as_view(), name='imageCode'),
+    path('imagecode/', ImageCodeView.as_view(), name='imagecode'),
+    # 短信验证码的路由
+    path('smscode/', SmsCodeView.as_view(), name='smscode'),
 ]
 # 2.在工程的urls.py总路由中添加子应用路由引导
