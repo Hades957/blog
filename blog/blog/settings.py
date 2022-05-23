4bb00f71651501e47c36c466f9b6431813892bf2
zhengbifeng <zhengbifeng@bz.cn>
2022-05-23 22:15:37 +0800

    1、博客栏目分类模型创建及应用注册

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 子应用的注册
     'users.apps.UsersConfig',
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
